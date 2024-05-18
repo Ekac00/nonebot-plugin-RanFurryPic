42abf44eaf48142dd2d679e89aa54686152e4a8a
XTxiaoting14332 <HT30332@126.com>
2024-05-18 21:17:52 +0800

    通过.env配置消息发送结果

--- nonebot_plugin_RanFurryPic/__init__.py
+++ nonebot_plugin_RanFurryPic/__init__.py
@@ -1,11 +1,12 @@
 from nonebot import on_command
-from nonebot import logger
+from nonebot import logger, get_plugin_config
 import httpx
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
 from nonebot.params import CommandArg
 from nonebot import require
 require("nonebot_plugin_saa")
 from nonebot_plugin_saa import Text, Image, MessageFactory
+from .config import Config
 
 
 __plugin_meta__ = PluginMetadata(
@@ -16,10 +17,14 @@ __plugin_meta__ = PluginMetadata(
     type="application",
 
     homepage="https://github.com/Ekac00/nonebot-plugin-RanFurryPic/",
-    # 发布必填。
 
     supported_adapters=inherit_supported_adapters("nonebot_plugin_session")
 )
+
+#读取配置
+config = get_plugin_config(Config)
+msg = config.ranfurrypic_msg
+
 
 furry = on_command("来只毛", aliases={"毛毛", "furry"}, priority=9, block=True)
 
@@ -53,10 +58,8 @@ async def handle_get_pic():
             logger.error(f"请求失败！\n状态码：{pic_response.status_code}")
             return f"请求失败！\n状态码：{pic_response.status_code}"
 
-    # 发送图片
     name = pic_json["picture"]["name"]
     suggest = pic_json["picture"]["suggest"]
-    #return MessageSegment.image(pic_id_url) + f"好的嗷呜～\n毛毛名称：{name}\n留言：{suggest}\n图片UID:{pic_id}"
 
     #使用saa构建消息
     return pic_id_url, name, suggest, pic_id
@@ -64,5 +67,5 @@ async def handle_get_pic():
 @furry.handle()
 async def handle_furry():
     pic_id_url, name, suggest, pic_id = await handle_get_pic()
-    text = f'好的嗷呜～\n毛毛名称：{name}\n留言：{suggest}\n图片UID:{pic_id}'
+    text = msg.replace('[NAME]',name).replace('[SUGGEST]',suggest).replace('[PICID]',pic_id)
     await MessageFactory([Text(text), Image(pic_id_url)]).send()
